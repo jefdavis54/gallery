3c0e27e19ed1a0d0969a65014052821122458176
Jeff Davis <jdavisnj@gmail.com>
2021-08-03 09:00:25 -0400

    Added styled-components to create-next-app --ts per offical with-styled-components example

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
+import { ThemeProvider } from "styled-components"
+import { GlobalStyle } from "../styles/a_global.sty"
+import { defaultTheme as theme } from "../styles/a_themes.sty"
 import type { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  )
 }
-export default MyApp
---
+++ styles/a_themes.sty.ts
@@ -0,0 +1,7 @@
+const defaultTheme = {
+  colors: {
+    primary: '#0070f3' 
+  }
+}
+
+export {defaultTheme}
